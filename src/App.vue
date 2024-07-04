d54943d9396dd1c639d53d885d9c1dd291ab5c93
gustavomurar <gustavomurarosoares3@gmail.com>
2024-07-04 15:37:14 -0300

    atua

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,120 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {reactive, ref} from 'vue'
+
+const produto= reactive({
+  nome: '',
+  idade: 1,
+  senha: '',
+  consenha: '',
+  cidade: '',
+z})
+
+const mostraResultado= ref(false)
+
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <h1>{{ titulo }}</h1>
+  <div class="contanier">
+    <div class="formulario">
+      <h2>Dados pessoais:</h2>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <form @submit.prevent="mostraResultado = !mostraResultado">
+      <div class="row">
+        <label for="">Nome:</label>
+        <input type="text" v-model="produto.nome" required/>
+      </div>
+
+      <div class="row">
+        <label for="">Idade:</label>
+        <input type="number" v-model="produto.preco"/>
+      </div>
+
+      <div class="row">
+        <label for="">Senha:</label>
+        <input type="password" v-model="produto.senha" />
+      </div>
+
+      <div class="row">
+        <label for="">Confirmar senha:</label>
+        <input type="password" v-model="produto.consenha" />
+      </div>
+
+      <div class="row">
+        <label for="">Email:</label>
+        <input type="email" v-model="produto.email" />
+      </div>
+
+
+      <div class="row">
+        <label for="">Data de nascimento:</label>
+        <input type="date" v-model="produto.consenha" />
+      </div>
+
+      <div class="row">
+        <label for="">Cidade:</label>
+        <input type="string" v-model="cidade" />
+      </div>
+
+      <div class="row">
+        <label for="">Estado:</label>
+        <input type="select" v-model="estado" />
+      </div>
+
+      <div class="row">
+        <label for="">Hobbies:</label>
+        <input type="text" v-model="nome" autocomplete="on" />
+      </div>
+
+      <div class="row">
+        <label for="">Biografia:</label>
+        <input type="text" v-model="nome" autocomplete="on" />
+      </div>
+
+      <div class="row">
+        <label for="">Estado:</label>
+        <input type="select" v-model="nome" autocomplete="on" />
+      </div>
+      
+      </form>
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+
+.contanier{
+display: flex;
+flex-direction: row;
+justify-content: center;
+gap: 1.5rom;
+width: 100w;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.formulario,
+.resultado {
+  background-color:rgb(255, 255, 255);
+  border-radius: 10px;
+  padding: 10px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.formulario{
+  background-color:rgba(201, 181, 70, 0.699);
 }
+
+.formulario.row{
+  margin: 10px auto;
+  width: 70%;
+  display: flex;
+  justify-content: space-between;
+}
+
+.resultado{
+  background-color:rgb(224, 131, 236);
+}
+
+
 </style>
